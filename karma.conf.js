cdd07cd44884dd82d9cadd1e010bf3e977374c58
smling <smling9_9@hotmail.com>
2022-08-12 22:27:22 +0100

    fix github action.

--- karma.conf.js
+++ karma.conf.js
@@ -30,15 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ],
-      check: {
-        global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
-        }
-      }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
